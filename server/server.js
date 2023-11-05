98ffdf474d7dedc3871e08575de772c0540aa99a
NavaHub <navaa003@gmail.com>
2023-11-05 00:18:07 -0500

    Manually installing GraphQL-related packages while working on server directory server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4')
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
