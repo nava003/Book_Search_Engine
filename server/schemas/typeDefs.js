92d3c5168d06277b60084fc6e9771aaf7e4248b3
NavaHub <navaa003@gmail.com>
2023-11-05 02:56:26 -0600

    typeDefs can now define types and be used for resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(userId: ID!, book: String!): User
+        removeBook(book: String!): User
+    }
+`;
+
+module.exports = typeDefs;
